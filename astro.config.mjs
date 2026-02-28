3d80b461532963fdf628db28eb39765023c31297
Abaho Katabarwa <abahomyster@gmail.com>
2026-02-28 07:29:38 -0800

    .gitignore is now working

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
-
-export default defineConfig({
-  site: "https://abahocodes.github.io",
-  integrations: [tailwind(), icon()],
-});
